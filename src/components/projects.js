d168a1f65b706bd154e835047ff3a5f833ab2880
NDo26 <do2026@uw.edu>
2024-10-20 01:15:20 -0700

    new project design

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,57 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PasswordProtectedPopup } from './passwordProtected'; // Import the password popup component
+import projectsData from '../projects.json'; // Import the projects JSON data
 
-export function Projects(props) {
+export function Projects() {
     const [isPopupVisible, setIsPopupVisible] = useState(false);
     const [currentProject, setCurrentProject] = useState(null);
 
-    const projects = [
-        {
-            title: "Ignite Robotics",
-            type: "Web Development • UI/UX • Internship",
-            description: "Website redesign and development of a student and administrative portal.",
-            attributes: ["Ongoing"],
-            imgSrc: './img/ignite.png',
-            link: "",
-            protected: true,
-        },
-        {
-            title: "Build Genie",
-            type: "Web Development",
-            description: "A web application that organizes and manages PC build parts in one place.",
-            attributes: ["React", "Javascript", "Firebase"],
-            imgSrc: './img/buildgenie.png',
-            link: "",
-            protected: true,
-        },
-        {
-            title: "Foodie Fun",
-            type: "Web Development",
-            description: "A web application that recommends, filters, and users to track and save their dining wishlist.",
-            attributes: ["Flask", "Python", "SQLAlchemy"],
-            imgSrc: './img/foodie.png',
-            link: "",
-            protected: true,
-        },
-        {
-            title: "Mylo",
-            type: "Product Design",
-            description: "Helping international students stay connected with their families, regardless of the distance.",
-            attributes: ["User Research", "Prototyping", "SolidWorks"],
-            imgSrc: './img/mylo.png',
-            link: "",
-            protected: true,
-        },
-        {
-            title: "Eco-Eye",
-            type: "UI/UX",
-            description: "A Google Chrome extension that helps users make informed, sustainable shopping decisions.",
-            attributes: ["User Research", "UI/UX", "Prototyping"],
-            imgSrc: './img/ecoeye.png',
-            link: "",
-            protected: true,
-        },
-    ];
+    const projects = projectsData;
 
     const handleProjectClick = (project) => {
         if (project.protected) {
@@ -59,7 +14,7 @@ export function Projects(props) {
             setIsPopupVisible(true);
         } else {
             if (project.link) {
-                window.open(project.link, '_blank'); // Open the link if it's not password-protected
+                window.open(project.link, '_blank');
             }
         }
     };
@@ -74,16 +29,12 @@ export function Projects(props) {
             >
                 <div className="project-img-container">
                     <img src={project.imgSrc} className="project-img" alt={project.title}/>
+                    
                 </div>
-                <h2 className="project-type">{project.type}</h2>
-                <p className="project-title">{project.title}</p>
-                <p>{project.description}</p>
-                <div className="project-attribute">
-                    {project.attributes.map((attribute, attrIndex) => (
-                        <div className="attribute" key={attrIndex}>
-                            <h2>{attribute}</h2>
-                        </div>
-                    ))}
+                <div className='project-text'>
+                    <p className="project-title" style={{ color: project['text-color']}}>{project.title}</p>
+                    <h2 className="project-type" style={{ color: project['text-color']}}>{project.type}</h2>
+                    <p className='project-description' style={{ color: project['text-color']}}>{project.description}</p>
                 </div>
             </div>
         ));
